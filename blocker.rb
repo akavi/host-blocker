1bf2be3ee2621da2c2d615663a3ad59a53d66a6e
Sherwin Yu <sherwin.x.yu@gmail.com>
2013-07-27 12:32:41 -0500

    initial commit

---
+++ blocker.rb
@@ -0,0 +1,31 @@
+regex = /^\s*#\s*(\S*)\s*(\S*)/
+raise "Need to specify an argumnt" if ARGV.empty?
+domain = ARGV.first
+wwwdomain = "www.#{domain}"
+
+def blockLine line, ip, dom
+  puts "BLOCKING LINE: #{line}"
+  "127.0.0.1  #{dom}\n"
+end
+
+
+f = File.open('/private/etc/hosts', 'r')
+
+newlines = f.lines.map do |line|
+  if (m = regex.match line)
+    ip, dom = m.captures
+    # p ip, dom
+    if [domain, wwwdomain].include? dom
+      line = blockLine line, ip, dom
+    end
+  end
+  line
+end
+f.close
+p newlines
+
+out = File.open('/private/etc/hosts', 'w')
+newlines.each do |line|
+  out.write line
+end
+out.close
