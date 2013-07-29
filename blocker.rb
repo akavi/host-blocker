febbce7b1ac9425d3e8d37f1eba860f5372ffc93
Arjun Kavi <Arjun@Arjuns-MacBook-Pro-2.local>
2013-07-28 20:33:20 -0700

    Adds support for append mode

--- blocker.rb
+++ blocker.rb
@@ -3,27 +3,48 @@
 regex = /^\s*#\s*(\S*)\s*(\S*)/
 hostfile_path = '/private/etc/hosts'
 
-def block_line line, dom
-  puts "BLOCKING LINE: #{line}"
+
+def block_line? line, doms
+  www_doms = doms.map { |d| "wwww.#{d}" } 
+  regex = /^\s*#\s*(\S*)\s*(\S*)/
+  m = regex.match line
+  found_dom = m ? m[2] : nil
+
+  blockable_dom = doms.find { |dom| dom == found_dom }
+  blockable_www_dom = www_doms.find { |dom| dom == found_dom }
+
+  blockable_dom or blockable_www_dom
+end
+
+def block_dom dom
   "127.0.0.1  #{dom}\n"
 end
 
 raise "Need to specify an argument" if ARGV.empty?
-domains = ARGV.map { |d| [d, "www.#{d}"] }.flatten
+flags, domains = ARGV.partition { |arg| arg.match("-.*") }
+append_mode = flags.include? "-a"
 
 f = File.open(hostfile_path, 'r')
 
 blocked_lines = []
 new_lines = f.lines.map do |line|
-  if (m = regex.match line)
-    ip, dom = m.captures
-    if domains.include? dom
-      line = block_line line, dom
-      blocked_lines << line
-    end
+  if (found_dom = block_line? line, domains)
+    puts "BLOCKING LINE: #{line}"
+    line = block_dom found_dom
+    blocked_lines << line
+    domains.delete found_dom.gsub(/^www\./, "")
   end
   line
 end
+
+if append_mode
+  domains.each do |domain|
+    line = block_dom domain
+    blocked_lines << line
+    new_lines << line
+  end
+end
+
 f.close
 
 puts "Blocked #{blocked_lines.count} line(s)", blocked_lines
